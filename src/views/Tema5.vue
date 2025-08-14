36662e12589e466cf8b514f4cb3c4dce8e5f6ba1
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-08-13 21:11:44 -0500

    tema 5

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -5,7 +5,115 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 5
-      h1 Tema 5
+      h1 Interacción
+
+    p.mb-4 La interacción en redes sociales se refiere a la participación de los usuarios con el contenido publicado por una marca, empresa o creador digital. Este concepto abarca acciones como comentarios, reacciones, compartidos y conversaciones dentro de la plataforma, convirtiéndose en un factor esencial para medir el impacto y la relevancia de una estrategia digital. La interacción es clave para fortalecer la relación entre una marca y su audiencia, sus beneficios incluyen:
+
+    .row.justify-content-center.align-items-center
+      .col-10.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema5/1.png', alt='imagen decorativa')
+      .col-lg-8
+        LineaTiempoD.color-secundario.custom-timeline
+          .row(numero="1" titulo="Generación de confianza")
+            p.text-small Una comunicación activa y transparente fomenta credibilidad y lealtad hacia la marca.
+
+          .row(numero="2" titulo="Fidelización de audiencia")
+            p.text-small La interacción frecuente fortalece el vínculo emocional entre los usuarios y la marca.
+
+          .row(numero="3" titulo="Conversión y <em>engagement</em>")
+            p.text-small Una mayor participación incrementa las probabilidades de convertir usuarios en clientes o seguidores.
+
+          .row(numero="4" titulo="Mayor alcance orgánico")
+            p.text-small Las plataformas priorizan y visibilizan el contenido con alto nivel de interacción.
+
+          .row(numero="5" titulo="Fidelización de audiencia")
+            p.text-small La retroalimentación directa permite conocer opiniones, preferencias y necesidades de la audiencia.
+
+    Separador
+
+    #t_5_1(data-aos='zoom-in-right').titulo-segundo.color-secundario.z-2
+      h3 5.1.	Principios
+
+    .row.justify-content-center.align-items-center.z-2.bg-color-secundario-s5.p-3.p-xl-1.rounded-4
+      .col-xl-9
+        p.mb-0 Para potenciar la participación de los usuarios, es importante aplicar ciertos principios, como la autenticidad la cual conlleva a responder de manera cercana y personalizada, evitando respuestas automáticas o genéricas, la rapidez y constancia para mantener una comunicación fluida y responder a los usuarios en tiempo oportuno, el incentivo a la conversación al hacer preguntas, crear encuestas y generar debates sobre temas relevantes, el por medio de publicaciones que inviten a la acción, como concursos, desafíos o dinámicas interactivas, y la empatía y escucha activa para lograr entender las inquietudes de los usuarios y responder de manera que fortalezca la conexión emocional.
+      .col-xl-3.d-none.d-xl-block
+        img(src="@/assets/curso/temas/tema5/2.png", alt="Imagen decorativa")
+
+    Separador
+
+    #t_5_2(data-aos='zoom-in-right').titulo-segundo.color-secundario.z-2
+      h3 5.2.	Fomento
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5
+      .col-10.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema5/3.png', alt='imagen decorativa')
+      .col-lg-8
+        p.mb-0 Fomentar la interacción en redes sociales no se trata solo de acumular comentarios y reacciones, sino de construir espacios de conversación genuinos. Cuando las marcas y creadores invitan a la participación con preguntas auténticas, responden con cercanía y generan contenido que conecta con las emociones de la audiencia, la interacción fluye de manera natural. Además, la constancia y la escucha activa fortalecen la comunidad, convirtiendo cada publicación en una oportunidad para compartir ideas, generar confianza y crear relaciones reales más allá de la pantalla. Al final, la clave está en recordar que detrás de cada like y comentario hay una persona con pensamientos, experiencias y ganas de ser parte de algo significativo. Algunas de las estrategias para aumentar la participación en redes sociales son:
+
+    .row.align-items-center.justify-content-center.z-2.mb-4.mb-lg-5
+      .col-lg-10
+        //- .titulo-sexto.color-adicional.mb-4.z-2
+        //-   h5 Tabla 2.
+        //-   span IA: aplicaciones, beneficios y retos éticos
+        .tabla-a.color-secundario.z-2
+          table
+            //- caption <strong>Fuente:</strong> SENA, 2025.
+            thead
+              tr(style="background-color: #CEDAF8;")
+                th Estrategia
+                th(colspan="3") Descripción
+            tbody
+              tr
+                td Publicaciones interactivas
+                td(colspan="3") Uso de encuestas, preguntas abiertas y dinámicas que inviten a comentar e interactuar.
+              tr(style="background-color: #DEE4F0;")
+                td Respuestas personalizadas
+                td(colspan="3") Responder comentarios con mensajes específicos en lugar de respuestas automáticas o genéricas.
+              tr
+                td Colaboraciones
+                td(colspan="3") Establecer alianzas con creadores de contenido o marcas complementarias para potenciar la visibilidad y enriquecer la interacción con nuevas audiencias.
+              tr(style="background-color: #DEE4F0;")
+                td <em>Hashtags</em> y tendencias
+                td(colspan="3") Integrar palabras clave y temas del momento para mejorar la visibilidad y conexión con el público.
+              tr
+                td Gamificación
+                td(colspan="3") Crear dinámicas con retos, premios y sistemas de participación que incentiven la interacción.
+    
+    Separador
+
+    #t_5_3(data-aos='zoom-in-right').titulo-segundo.color-secundario.z-2
+      h3 5.3.	Medición de participación
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5
+      .col-lg-8
+        p.mb-4.mb-lg-0 Medir la participación en redes sociales es una oportunidad para descubrir qué inspira, emociona y conecta con la audiencia. Más allá de los “likes” o comentarios, se trata de comprender cómo las personas interactúan con el contenido, qué conversaciones nacen y qué valor se genera en cada publicación. Al observar métricas como el tiempo de visualización, la cantidad de respuestas o los contenidos compartidos, es posible identificar lo que realmente resuena y fortalece el vínculo con la comunidad. Lo más valioso no siempre está en la cantidad, sino en la calidad de las interacciones: cuando una publicación despierta diálogo, moviliza o deja huella, su impacto se multiplica. Medir la participación, entonces, es aprender en tiempo real, adaptarse con empatía y construir relaciones genuinas en el mundo digital, de acuerdo con:
+      .col-10.col-lg-4
+        figure
+          img(data-aos="fade-up")(src='@/assets/curso/temas/tema5/4.png', alt='imagen decorativa')
+
+    .row.justify-content-center.align-items-center.z-2
+      .col-8.col-sm-6.col-xl-4.d-none.d-xl-block
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down")(src='@/assets/curso/temas/tema5/5.png', alt='Imagen decorativa')
+      .col-xl-8
+        AcordionA(tipo="a" clase-tarjeta="tarjeta acordion-conservas")
+          div(titulo="Cantidad de comentarios, reacciones y compartidos")
+            p Indica el nivel de participación del público.
+
+          div(titulo="Tiempo de respuesta")
+            p Mide la rapidez con la que se gestiona la interacción con los usuarios.
+
+          div(titulo="Tasa de <em>engagement</em>")
+            p Relación entre el número de interacciones y el total de seguidores o alcance de la publicación.
+
+          div(titulo="Menciones y conversaciones")
+            p Cantidad de veces que una marca o usuario es mencionado en redes.
+
+          div(titulo="Análisis de sentimiento")
+            p Evaluación de si las interacciones son positivas, negativas o neutrales.
 
 
 </template>
