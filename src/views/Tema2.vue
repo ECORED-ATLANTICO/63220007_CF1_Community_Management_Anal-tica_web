4fedd6f3f67f0b56c3f1310692079aa687f662bf
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-08-14 00:16:01 -0500

    tema2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,7 +5,165 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Tema 2
+      h1 Plan de redes sociales
+
+    img(data-aos="fade-up")(src="@/assets/curso/temas/tema2/1.png", alt="Imagen decorativa")
+
+    .row.justify-content-center.align-items-center.z-2
+      .col-lg-10
+        p.banner-text Un plan de redes sociales es la estrategia diseñada para gestionar la presencia digital de una marca, empresa o persona en plataformas sociales. Este plan permite establecer objetivos, seleccionar las redes adecuadas, definir el tipo de contenido y desarrollar acciones que generen interacción con la audiencia, fortaleciendo la identidad digital y el posicionamiento en el mercado.
+
+    Separador
+
+    #t_2_1(data-aos='zoom-in-right').titulo-segundo.color-secundario.z-2
+      h3 2.1.	Elementos
+
+    .fullwidth-bg.fullwidth-bg--2
+    .row.justify-content-center.align-items-center.z-2.mb-4.mb-lg-5
+      .col-lg-8
+        .cajon.color-terciario.p-4.mb-4.mb-lg-0(data-aos="fade-left")
+          p.mb-0 Para que un plan de redes sociales sea efectivo, debe incluir varios elementos clave. Su estructura puede variar según la experiencia y enfoque de cada persona, pero hay ciertos componentes que suelen ser fundamentales. A continuación, presentamos los más comunes, tales como los objetivos, por medio de los cuales se define qué se busca lograr, ya sea aumentar la visibilidad, generar engagement o impulsar ventas, la audiencia para identificar el público objetivo y sus intereses para adaptar la estrategia, las plataformas, para elegir las redes más adecuadas según el tipo de negocio y audiencia, el contenido para planificar qué tipo de publicaciones se realizarán y con qué frecuencia y las métricas y análisis, para evaluar el rendimiento mediante herramientas de medición con el fin de hacer ajustes estratégicos en la medida que se requieran.
+      .col-10.col-sm-8.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema2/2.png', alt='imagen decorativa')
+
+    .row.justify-content-center.align-items-center.z-2.mb-4.mb-lg-5
+      .col-12.col-lg-6
+        .titulo-sexto.color-adicional.mb-4
+          h5 Figura 3.
+          span Acciones de adaptabilidad
+        img(data-aos="fade-up")(src="@/assets/curso/temas/tema2/3.png", alt="Escuchar a las audiencias. Analizar comentarios y métricas para mejorar. Diversificar formatos. Explorar nuevos tipos de contenido como reels, podcasts o carruseles. Actualización permanente. Seguir tendencias y cambios en el comportamiento digital. Optimizar plataformas. Cada red social tiene su propio estilo y formato ideal.")
+        figcaption <strong>Fuente:</strong> SENA, 2025.
+
+    Separador
+    #t_2_2(data-aos='zoom-in-right').titulo-segundo.color-secundario.z-2
+      h3 2.2. Características
+
+    p.mb-4.mb-lg-5 Un plan de redes sociales efectivo debe reunir características que garanticen su eficiencia, adaptabilidad y alineación con los objetivos de marketing. A continuación, se presentan los elementos clave que deben considerarse en su formulación para lograr una gestión digital estratégica y coherente:
+
+    .row.justify-content-center.align-items-center.z-2.mb-4.mb-lg-5
+      .col-8.col-lg-3
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema2/4.png', alt='imagen decorativa')
+      .col-lg-9
+        LineaTiempoD.color-secundario.custom-timeline
+          .row(numero="1" titulo="Flexible")
+            p.text-small Ajustable a cambios en tendencias y comportamiento de la audiencia.
+
+          .row(numero="2" titulo="Medible")
+            p.text-small Basado en datos y métricas para evaluar el impacto de cada acción.
+
+          .row(numero="3" titulo="Creativo")
+            p.text-small Aplicar formatos innovadores y contenido atractivo para captar la atención.
+
+          .row(numero="4" titulo="Interactivo")
+            p.text-small Fomentar la participación de la audiencia mediante respuestas, encuestas y dinámicas.
+
+          .row(numero="5" titulo="Coherente")
+            p.text-small Mantener una línea visual y comunicativa alineada con la marca.
+
+    Separador
+    
+    #t_2_3(data-aos='zoom-in-right').titulo-segundo.color-secundario.z-2
+      h3 2.3. Estructura
+
+    p.mb-4.mb-lg-5 La estructura de un plan de redes sociales responde a un orden lógico que busca garantizar su efectividad. No obstante, su diseño puede variar según la experiencia, el enfoque y la perspectiva del profesional que lo elabore. A continuación, se presenta un orden sugerido que puede servir como guía para su implementación.
+    
+    .row.justify-content-center.align-items-center.z-2.mb-4.mb-lg-5
+      .col-10.col-xl-4.d-none.d-xl-block
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema2/5.png', alt='imagen decorativa')
+      .col-xl-8
+        .tarjeta.conservas-slyder.p-4.p-lg-5.pb-lg-4
+          SlyderA(tipo="b")
+            .row.justify-content-center.align-items-center
+              .col-lg-9.mb-4.mb-lg-0
+                h5.mb-4 1. Análisis inicial
+                p La automatización facilita la publicación de contenidos en momentos clave, maximizando el alcance.Investigación del mercado y diagnóstico de la situación actual en redes.
+              .col-4.col-lg-2
+                figure
+                  img.w-72(data-aos="fade-up")(src='@/assets/curso/temas/tema2/6.png', alt='imagen decorativa')
+
+            .row.justify-content-center.align-items-center
+              .col-lg-9.mb-4.mb-lg-0
+                h5.mb-4 2. Definición de objetivos
+                p Establecimiento de metas SMART (específicas, medibles, alcanzables, relevantes y con tiempo definido).
+              .col-4.col-lg-2
+                figure
+                  img.w-72(data-aos="fade-up")(src='@/assets/curso/temas/tema2/7.png', alt='imagen decorativa')
+
+            .row.justify-content-center.align-items-center
+              .col-lg-9.mb-4.mb-lg-0
+                h5.mb-4 3. Segmentación de audiencia.
+                p Identificación del público objetivo y estudio de sus intereses.
+              .col-4.col-lg-2
+                figure
+                  img.w-72(data-aos="fade-up")(src='@/assets/curso/temas/tema2/8.png', alt='imagen decorativa')
+
+            .row.justify-content-center.align-items-center
+              .col-lg-9.mb-4.mb-lg-0
+                h5.mb-4 4. Selección de plataformas
+                p Elección de redes sociales en función de la estrategia y tipo de contenido.
+              .col-4.col-lg-2
+                figure
+                  img.w-72(data-aos="fade-up")(src='@/assets/curso/temas/tema2/9.png', alt='imagen decorativa')   
+            
+            .row.justify-content-center.align-items-center
+              .col-lg-9.mb-4.mb-lg-0
+                h5.mb-4 5. Desarrollo de contenido
+                p Creación del calendario de publicaciones y formatos adecuados.
+              .col-4.col-lg-2
+                figure
+                  img.w-72(data-aos="fade-up")(src='@/assets/curso/temas/tema2/10.png', alt='imagen decorativa')
+            
+            .row.justify-content-center.align-items-center
+              .col-lg-9.mb-4.mb-lg-0
+                h5.mb-4 6. Implementación de estrategias
+                p Aplicación de acciones específicas para generar interacción y <em>engagement</em>.
+              .col-4.col-lg-2
+                figure
+                  img.w-72(data-aos="fade-up")(src='@/assets/curso/temas/tema2/11.png', alt='imagen decorativa')
+            
+            .row.justify-content-center.align-items-center
+              .col-lg-9.mb-4.mb-lg-0
+                h5.mb-4 7. Medición y ajustes
+                p Evaluación de métricas y adaptación de la estrategia según los resultados obtenidos.
+              .col-4.col-lg-2
+                figure
+                  img.w-72(data-aos="fade-up")(src='@/assets/curso/temas/tema2/12.png', alt='imagen decorativa')
+
+    .row.justify-content-center.align-items-center.z-2.bg-gradiente.rounded-4.p-4.mb-4.mb-lg-5
+      .col-lg-10
+        h5.color-acento-botones ¿Cómo se hace un plan de redes sociales?
+        p.text-white A continuación, se presenta un video que muestra, de forma práctica, un ejemplo de elaboración de un plan de redes sociales. Este recurso destaca cómo, dependiendo del contexto y los objetivos específicos, es posible adaptar e integrar otros elementos según las particularidades de cada estrategia.
+      .col-lg-2
+        .d-flex
+          a.boton.color-acento-botones.mx-auto(:href="('https://youtu.be/KXd6BioXc8c?si=xFDyXgnoglBRgcPV')" target="_blank")
+            strong Video
+            i.ps-1.fas.fa-link
+
+    .row.align-items-center.justify-content-center.mb-4.mb-lg-5
+      .col-10.col-sm-8.col-lg-2
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema2/13.png', alt='imagen decorativa')
+      .col-lg-10
+        p En la actualidad, los avances tecnológicos y el acceso a Internet han dado lugar al desarrollo de múltiples plataformas que facilitan la planificación y gestión de estrategias en redes sociales. Estas herramientas ofrecen a profesionales, emprendedores y empresas la posibilidad de estructurar contenidos, definir objetivos y optimizar sus procesos de publicación de forma eficiente.
+
+    .row.justify-content-center.align-items-center.z-2.mb-4.mb-lg-5
+      .col-lg-8.mb-4.mb-lg-0
+        .cajon.color-terciario.p-4(data-aos="fade-left")
+          p.mb-0 A continuación, se presenta una de estas plataformas, reconocida por su facilidad de uso y versatilidad. En ella, se encuentra disponible una plantilla diseñada para orientar la elaboración de un plan de redes sociales. Esta plantilla puede ser diligenciada en línea, lo que permite personalizar cada sección de acuerdo con las necesidades y estrategias particulares. Se invita al aprendiz a explorar este recurso, el cual puede convertirse en un aliado fundamental para fortalecer y organizar su presencia digital.
+      .col-lg-4
+        .row.bg-gradiente.rounded-4.p-4.align-items-center
+          .col-9
+            h5.mb-4.color-acento-botones Diligenciamiento de un plan de redes sociales
+            a.boton.color-acento-botones.mx-auto.mb-2(:href="('https://www.wonder.legal/es/creation-modele/plan-redes-sociales-18')" target="_blank")
+              strong Diligenciar
+              i.ps-1.fas.fa-link
+          .col-3
+            figure
+              img(data-aos="fade-down")(src='@/assets/curso/temas/tema2/14.png', alt='imagen decorativa')
+
 
 </template>
 
