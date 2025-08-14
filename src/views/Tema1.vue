d47581813d2b06c3f794852f89b8268e9ba780f4
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-08-14 07:36:58 -0500

    tema1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -6,7 +6,241 @@
       .titulo-principal__numero
         span 1
       h1 Ecosistema digital
- 
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-10.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema1/1.png', alt='imagen decorativa')
+
+      .col-lg-8
+        p.mb-4 En las últimas décadas, el acelerado avance de la tecnología y la expansión de Internet dieron lugar al concepto de "ecosistema digital", una estructura que, al igual que un ecosistema biológico, está compuesta por elementos interconectados que interactúan de manera dinámica.
+        p Este entorno digital engloba plataformas, usuarios, herramientas y procesos que evolucionan constantemente para adaptarse a nuevas necesidades y oportunidades, se basan en el conocimiento técnico, y pueden ser consideradas como “arquitecturas robustas, autoorganizadas y escalables que pueden resolver automáticamente problemas complejos y dinámicos” (Briscoe y Wilde 2009).
+
+    .cajon.color-secundario.p-4.mb-4.mb-lg-0(data-aos="fade-left")
+      p.mb-0 En el ámbito del <em>marketing</em> digital, este ecosistema ha revolucionado la manera en que las marcas se relacionan con su público, permitiendo una interacción más directa, personalizada y medible. Gracias a herramientas como la automatización, la analítica de datos y la inteligencia artificial, las empresas pueden segmentar audiencias con mayor precisión, optimizar campañas en tiempo real y mejorar la experiencia del usuario. Este impacto ha convertido al ecosistema digital en el eje central de la estrategia comercial moderna, donde la adaptabilidad y la innovación son esenciales para el crecimiento y posicionamiento en el mercado. 
+
+    Separador
+
+    #t_1_1(data-aos='zoom-in-right').titulo-segundo.color-secundario.z-2
+      h3 1.1.	Redes sociales
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-8.col-lg-3
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema1/2.png', alt='imagen decorativa')
+
+      .col-lg-9
+        p.mb-4 Las redes sociales han revolucionado la forma en que las personas interactúan en el mundo digital. Estas plataformas permiten la conexión entre individuos, marcas y comunidades, facilitando la comunicación en tiempo real a través de distintos formatos como texto, imágenes, videos y transmisiones en vivo. Más que simples espacios de entretenimiento, las redes sociales han evolucionado para convertirse en herramientas esenciales de interacción y difusión de contenido a gran escala. Según Coiera (2013) las redes sociales son medios de comunicación, diferentes a los tradicionales, ya que utilizan un conjunto de tecnologías, con el fin de crear espacios virtuales para que los usuarios puedan interactuar entre sí.
+        p.bg-color-acento-contenido-a5.rounded-4.p-4 Las redes sociales, también conocidas como social media, Campoverde (2021) las identifica como un aliado estratégico de las empresas, para poder ser generadoras de ventas e ingresos, así mismo, son destacadas como las más importantes estrategias del marketing digital, por medio de las cuales se potencializa la publicidad, propiciando ganancias, tanto en micro, pequeñas y medianas empresas.
+
+    .row.justify-content-center.align-items-center.z-2.bg-gradiente.rounded-4.p-4.mb-4.mb-lg-5
+      .col-lg-9
+        h5.text-white A continuación, un video donde se aprecia el tipo de redes sociales.
+        p.text-white <strong>Descripción</strong>: video del Ecosistema de Recursos Educativos Digitales SENA, en el cual se exponen los perfiles digitales en comunidades en línea, tipos de redes sociales.
+      .col-lg-3
+        .d-flex
+          a.boton.color-acento-botones.mx-auto(:href="('https://www.youtube.com/watch?v=tdEv-yO1Uvg')" target="_blank")
+            strong Tipos de redes sociales
+            i.ps-1.fas.fa-link
+
+    h3.graph-title.my-5(data-aos="fade-right") Evolución
+
+    p.mb-4.mb-lg-5 Desde sus inicios las redes sociales han experimentado una transformación significativa posibilitando nuevas formas de comunicación digital (Opresmedia, 2023). Pasaron de ser sitios para compartir experiencias personales a espacios estratégicos donde empresas, creadores de contenido y organizaciones construyen presencia digital, desarrollan comunidades y generan impacto social. La evolución tecnológica ha permitido integrar algoritmos avanzados que optimizan el alcance del contenido, segmentan audiencias y potencian el engagement de manera más eficiente.
+
+    .row.justify-content-center.align-items-center.z-2.mb-4.mb-lg-5
+      .col-12
+        .titulo-sexto.color-adicional.mb-4
+          h5 Figura 1.
+          span Línea de tiempo de las redes sociales.
+        img.d-md-none(data-aos="fade-up")(src="@/assets/curso/temas/tema1/5-mobile.png", alt="En la figura se muestra una línea de tiempo de once momentos de las redes sociales desde 1996 hasta 2016.")
+        img.d-none.d-md-block.mb-1(data-aos="fade-up")(src="@/assets/curso/temas/tema1/5.png", alt="En la figura se muestra una línea de tiempo de once momentos de las redes sociales desde 1996 hasta 2016.")
+        figcaption <strong>Fuente:</strong> SENA, 2025.
+
+    .fullwidth-bg.fullwidth-bg--1
+    h3(data-aos="fade-right").graph-title.my-5.z-2 Aplicabilidad en el <em>marketing</em>
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-10.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema1/6.png', alt='imagen decorativa')
+
+      .col-lg-8
+        p.mb-4.mb-lg-0 En el ámbito del <em>marketing</em> digital, las redes sociales desempeñan un papel crucial en la promoción de productos, servicios y marcas. Gracias a la posibilidad de segmentar audiencias, analizar métricas en tiempo real y personalizar campañas, las empresas pueden conectar con sus clientes de manera más directa y efectiva, pueden mejorar las ventas, construir y reforzar sus marcas, y crear vínculos con los clientes (Mera et al., 2022). La automatización y el análisis de datos han optimizado estrategias como la publicidad pagada, el <em>influencer marketing</em> y la creación de contenido viral, logrando una mayor conversión y fidelización en el entorno digital.
+
+    h3(data-aos="fade-right").graph-title.my-5.z-2 Atributos
+
+    p.mb-4.mb-lg-5.z-2 Las redes sociales, en su diversidad de plataformas y propósitos, comparten una serie de atributos que definen su funcionalidad y valor estratégico. Comprender estos atributos permite identificar cómo se facilita la interacción, la difusión de contenido y la construcción de comunidades digitales activas y participativas:
+
+    .row.justify-content-center.z-2.mb-4.mb-lg-5
+      .col-10.col-md-6.col-lg-4.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/tema1/7.png", alt="Imagen decorativa")
+          .crd_hover_txt--body.text-center
+            h5.mb-3 Interactividad
+            p.mb-0 Permiten la comunicación bidireccional entre usuarios y marcas, facilitando la participación y el intercambio de información.
+      .col-10.col-md-6.col-lg-4.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/tema1/8.png", alt="Imagen decorativa")
+          .crd_hover_txt--body.text-center
+            h5.mb-3 Viralidad
+            p.mb-0 El contenido puede difundirse rápidamente a través de compartidos, comentarios y reacciones, alcanzando grandes audiencias en poco tiempo.
+      .col-10.col-md-6.col-lg-4.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/tema1/9.png", alt="Imagen decorativa")
+          .crd_hover_txt--body.text-center
+            h5.mb-3 Interactividad
+            p.mb-0 Permiten la comunicación bidireccional entre usuarios y marcas, facilitando la participación y el intercambio de información.
+
+    .row.justify-content-center.align-items-center.z-2.mb-4.mb-lg-5
+      .col-lg-1.d-none.d-xl-block
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema1/10.png', alt='imagen decorativa')
+      .col-xl-11
+        p.mb-0 Estos atributos permiten a los gestores de comunidades digitales tomar decisiones más informadas y estratégicas. La personalización, la medición y el análisis, junto con la automatización, se convierten en herramientas clave para mejorar la experiencia del usuario, optimizar recursos y fortalecer la presencia de marca en entornos digitales.
+
+    .row.justify-content-center.align-items-center.z-2.mb-4.mb-lg-5
+      .col-8.col-sm-6.col-xl-4.d-none.d-xl-block
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down")(src='@/assets/curso/temas/tema1/11.png', alt='Imagen decorativa')
+      .col-xl-8
+        AcordionA(tipo="a" clase-tarjeta="tarjeta acordion-conservas")
+          div(titulo="Personalización")
+            p Adaptan la experiencia del usuario mediante algoritmos que muestran contenido relevante según sus preferencias y comportamiento.
+
+          div(titulo="Medición y análisis")
+            p Proporcionan métricas detalladas sobre el rendimiento de publicaciones, interacción de usuarios y efectividad de campañas.
+
+          div(titulo="Automatización")
+            p Facilitan la gestión de contenido y la programación de publicaciones para optimizar la presencia digital de una marca.
+
+    h3(data-aos="fade-right").graph-title.my-5.z-2 Principales plataformas
+
+    .fullwidth-bg.fullwidth-bg--1
+    p.mb-4.mb-lg-5.z-2 Cada una de las plataformas tiene características únicas que pueden ser aprovechadas según los objetivos de <em>marketing</em> y el tipo de audiencia, a continuación, apreciaremos las plataformas que han logrado tener un mayor uso.
+
+    .tarjeta.conservas-slyder.p-4.p-lg-5.pb-lg-4.z-2.mb-4.mb-lg-5.shadow
+      SlyderA(tipo="b")
+        .row.justify-content-center.align-items-center
+          .col-lg-7.mb-4.mb-lg-0
+            h5.mb-4 Facebook
+            p La red social más grande del mundo, ideal para conectar con diversas audiencias. Ofrece herramientas de publicidad avanzada, grupos, páginas y transmisiones en vivo, permitiendo a las marcas construir comunidades y fortalecer su presencia digital.
+          .col-10.col-sm-8.col-lg-4
+            figure
+              img(data-aos="fade-up")(src='@/assets/curso/temas/tema1/12.png', alt='imagen decorativa')
+
+        .row.justify-content-center.align-items-center
+          .col-lg-7.mb-4.mb-lg-0
+            h5.mb-4 Instagram
+            p Enfocada en contenido visual, es perfecta para compartir imágenes y videos. Su algoritmo favorece el engagement a través de historias, reels y publicaciones interactivas. Es clave para estrategias de branding y marketing de influencers.
+          .col-10.col-sm-8.col-lg-4
+            figure
+              img(data-aos="fade-up")(src='@/assets/curso/temas/tema1/13.png', alt='imagen decorativa')
+
+        .row.justify-content-center.align-items-center
+          .col-lg-7.mb-4.mb-lg-0
+            h5.mb-4 X (Twitter)
+            p Plataforma de microblogging que destaca por su inmediatez y capacidad de generar conversaciones en tiempo real. Es utilizada para noticias, tendencias y comunicación directa entre marcas y usuarios.
+          .col-10.col-sm-8.col-lg-4
+            figure
+              img(data-aos="fade-up")(src='@/assets/curso/temas/tema1/14.png', alt='imagen decorativa')
+
+        .row.justify-content-center.align-items-center
+          .col-lg-7.mb-4.mb-lg-0
+            h5.mb-4 LinkedIn
+            p La red profesional por excelencia, ideal para networking, reclutamiento y generación de contenido corporativo. Su enfoque permite a empresas y profesionales compartir conocimientos y fortalecer su presencia en el ámbito laboral.
+          .col-10.col-sm-8.col-lg-4
+            figure
+              img(data-aos="fade-up")(src='@/assets/curso/temas/tema1/15.png', alt='imagen decorativa')   
+        
+        .row.justify-content-center.align-items-center
+          .col-lg-7.mb-4.mb-lg-0
+            h5.mb-4 TikTok
+            p Centrada en videos cortos y contenido viral, es una de las plataformas con mayor crecimiento. Su algoritmo favorece la creatividad y el descubrimiento de contenido, siendo clave para estrategias de marketing digital dirigidas a audiencias jóvenes.
+          .col-10.col-sm-8.col-lg-4
+            figure
+              img(data-aos="fade-up")(src='@/assets/curso/temas/tema1/16.png', alt='imagen decorativa')
+        
+        .row.justify-content-center.align-items-center
+          .col-lg-7.mb-4.mb-lg-0
+            h5.mb-4 YouTube
+            p La plataforma líder en contenido de video, utilizada tanto para entretenimiento como para educación y marketing. Su modelo de monetización y alcance global la convierten en una herramienta poderosa para creadores y marcas.
+          .col-10.col-sm-8.col-lg-4
+            figure
+              img(data-aos="fade-up")(src='@/assets/curso/temas/tema1/17.png', alt='imagen decorativa')
+        
+        .row.justify-content-center.align-items-center
+          .col-lg-7.mb-4.mb-lg-0
+            h5.mb-4 Pinterest
+            p Enfocada en la inspiración visual, es ideal para compartir ideas sobre moda, decoración, recetas y proyectos creativos. Su sistema de tableros permite a los usuarios organizar contenido y descubrir tendencias.
+          .col-10.col-sm-8.col-lg-4
+            figure
+              img(data-aos="fade-up")(src='@/assets/curso/temas/tema1/18.png', alt='imagen decorativa')
+
+    .row.justify-content-center.align-items-center.mb-4.z-2
+      .col-10.col-sm-6.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema1/19.png', alt='imagen decorativa')
+
+      .col-lg-8
+        .row.p-4.bg-color-terciario-t5.rounded-4.align-items-center.mb-4.shadow
+          .col-2.d-none.d-xl-block
+            figure
+              img.w-72(data-aos="fade-up")(src='@/assets/curso/temas/tema1/20.png', alt='imagen decorativa')
+          .col-xl-10
+            h5 Redes sociales horizontales.
+            p Enfocadas en conectar a usuarios de diversas edades y contextos, permitiendo la difusión masiva y la interacción generalizada. (Opresmedia, 2023)
+        .row.p-4.bg-color-terciario-t5.rounded-4.align-items-center.shadow
+          .col-2.d-none.d-xl-block
+            figure
+              img.w-72(data-aos="fade-up")(src='@/assets/curso/temas/tema1/20.png', alt='imagen decorativa')
+          .col-xl-10
+            h5 Redes sociales verticales.
+            p Especializadas en nichos específicos; por ejemplo, LinkedIn se centra en el ámbito profesional y Pinterest en intereses visuales y creativos. (Lara, 2023)
+
+    Separador
+
+    #t_1_2(data-aos='zoom-in-right').titulo-segundo.color-secundario.z-2
+      h3 1.2.	Audiencias
+
+    .row.justify-content-center.align-items-center.z-2.mb-4.mb-lg-5
+      .col-12.col-lg-8
+        p Las audiencias representan el grupo de personas a las que va dirigido un mensaje, producto o servicio. En <em>marketing</em> digital, estas audiencias son fundamentales para la creación de estrategias efectivas, ya que conocer sus intereses, comportamientos y necesidades permite diseñar campañas personalizadas y aumentar la conexión con los usuarios.
+        .cajon.color-secundario.p-4.mb-4.mb-lg-0(data-aos="fade-left")
+          p.mb-0 Las audiencias digitales tienen particularidades que las diferencian de otros tipos de públicos, comprender estos aspectos es esencial para diseñar estrategias que generen impacto y logren una conexión efectiva con los usuarios, algunos de ellos son:
+      .col-10.col-sm-8.col-lg-4.d-none.d-lg-block
+        figure
+          img(data-aos="fade-left")(src='@/assets/curso/temas/tema1/21.png', alt='imagen decorativa')
+
+    .row.justify-content-center.align-items-center.z-2.mb-4.mb-lg-5
+      .col-8.col-sm-6.col-lg-3
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema1/22.png', alt='imagen decorativa')
+      .col-lg-9
+        LineaTiempoD.color-secundario.custom-timeline
+          .row(numero="1" titulo="Interactividad")
+            p.text-small Pueden responder, comentar y compartir contenido en tiempo real.
+
+          .row(numero="2" titulo="Conectividad constante")
+            p.text-small Acceden desde múltiples dispositivos y plataformas.
+
+          .row(numero="3" titulo="Exigencia de personalización")
+            p.text-small Esperan contenido relevante y adaptado a sus intereses.
+
+          .row(numero="4" titulo="Influenciabilidad")
+            p.text-small Sus decisiones pueden verse afectadas por opiniones de otros usuarios y tendencias digitales.
+
+          .row(numero="5" titulo="Volatilidad")
+            p.text-small Cambian de preferencias rápidamente según la actualidad y el contexto.
+
+    .cajon.color-acento-contenido.p-4.mb-4.mb-lg-0
+      p.mb-0 Las audiencias pueden dividirse en segmentos, que son grupos específicos de personas con características comunes. Algunos criterios de segmentación incluyen elementos demográficos tales como la edad, género, nivel educativo y ubicación, entre otros, elementos psicográficos como los intereses, valores y estilo de vida, elementos conductuales como los hábitos de consumo, frecuencia de compra e interacción con la marca y elementos geográficos como el país, ciudad, clima o ubicación determinada. Cada segmento permite personalizar campañas para lograr una mayor relevancia y eficacia en la comunicación con el público objetivo.
+
 </template>
 
 <script>
